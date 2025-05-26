e8940ef537a925bfd7c2da0a37c2893ef08c585d
georodrigues.linda <georodrigues.linda@gmail.com>
2025-05-26 11:47:17 +0000

    criação do arquivo main.js, criação do array de items e criação da função para adicionar novos itens ao array

---
+++ main.js
@@ -0,0 +1,15 @@
+const items = []
+
+function addItem() {
+ const itemName = document.querySelector("#item").value
+
+const item = {
+    name: itemName;
+    checked: false 
+}
+
+items.push(item)
+
+document.querySelector("#item").value = ""
+console.log(items)
+}
