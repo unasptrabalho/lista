cf7d617101610a6e8ac1ebe44e829d0fdf73ec22
georodrigues.linda <georodrigues.linda@gmail.com>
2025-05-30 00:24:46 +0000

    criação da função para remover os itens do array: removeItems

--- main.js
+++ main.js
@@ -26,10 +26,23 @@ sectionList.textContent = ""
    </div>
 <label for="item-${index}">${item.name}</label>
 </div>
-<button>
+<button onclick="removeItem('${item.name}')">
     <img src="./assets/trash-icon.svg" alt="trash-icon">
 </button>
 </div>
 `
 })
+}
+
+function removeItem(itemName) {
+    const itemIndex = items.findIndex((item) => item.name === itemName)
+   const divWarning = document.querySelector(".warning")
+
+divWarning.classList.remove("hide-warning")
+
+    if (itemIndex !== -1) {
+        items.splice(itemIndex, 1)
+    }
+
+    showItemsList()
 }
